9b228a3c573a42c3554e1947ca23fc03ea46f11f
support1122 <support@flashfirejobs.com>
2025-12-21 12:39:24 +0530

    Update page.tsx

--- app/testimonials/page.tsx
+++ app/testimonials/page.tsx
@@ -22,16 +22,16 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "https://www.flashfirejobs.com/favicon.svg",
-        width: 803,
-        height: 781,
+        url: "https://www.flashfirejobs.com/images/flashfire-logo.png",
+        width: 1200,
+        height: 630,
         alt: "FLASHFIRE Logo",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    images: ["https://www.flashfirejobs.com/favicon.svg"],
+    images: ["https://www.flashfirejobs.com/images/flashfire-logo.png"],
   },
 };
 
