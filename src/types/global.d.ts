54f81a300df635f14e7a78582714fd0a6b2adec6
support1122 <support@flashfirejobs.com>
2026-03-03 16:08:03 +0530

    Update global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -12,5 +12,13 @@ interface Window {
     params?: Record<string, any>
   ) => void;
   _fbq?: any;
+  lintrk?: ((
+    command: "track" | string,
+    params?: Record<string, any>
+  ) => void) & {
+    q?: Array<[string, Record<string, any>]>;
+  };
+  _linkedin_partner_id?: string;
+  _linkedin_data_partner_ids?: string[];
 }
 
