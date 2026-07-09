d9c4ef576106e423bb29ed1a30678b438673fa9b
support1122 <support@flashfirejobs.com>
2026-07-09 21:43:24 +0530

    Update CalendlyModal.tsx

--- src/components/calendlyModal/CalendlyModal.tsx
+++ src/components/calendlyModal/CalendlyModal.tsx
@@ -446,7 +446,7 @@ max-w-5xl w-full max-h-[90vh] overflow-hidden rounded-xl shadow-2xl flex flex-co
                 backgroundColor: "ffffff",
                 hideEventTypeDetails: false,
                 hideLandingPageDetails: false,
-                primaryColor: "eaeaea",
+                primaryColor: "f97316",
                 textColor: "374151",
               }}
             />
